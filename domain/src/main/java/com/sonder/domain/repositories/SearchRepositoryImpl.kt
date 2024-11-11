2624d11c213b758d8601e7e2f43622ca896709c8
Jun <leej1025y@live.com>
2024-11-11 22:52:26 +1100

    feat: SearchRepository stbu

--- domain/src/main/java/com/sonder/domain/repositories/SearchRepositoryImpl.kt
+++ domain/src/main/java/com/sonder/domain/repositories/SearchRepositoryImpl.kt
@@ -1,14 +1,28 @@
 package com.sonder.domain.repositories
 
+import com.sonder.domain.models.SearchSectionResult
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class SearchRepositoryImpl @Inject constructor() : SearchRepository {
 
-    override val searchQuery: Flow<String>
-        get() = TODO("Not yet implemented")
+    private val _searchQuery = MutableStateFlow("")
+    override val searchQuery = _searchQuery.asStateFlow()
+
+    override suspend fun search(query: String): Flow<Result<List<SearchSectionResult>>> = flow {
+        delay(2000L) // Simulate network delay
+        emit(
+            if (query == "error") Result.failure(Exception("Error fetching results"))
+            else Result.success(emptyList())
+        )
+    }
 
     override fun clearSectionSearchResults() {
-        TODO("Not yet implemented")
+        _searchQuery.value = ""
     }
+
 }
